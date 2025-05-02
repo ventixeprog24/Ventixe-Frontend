398a0eed8b1a120c44bc9a07322a21d652a4efe1
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2025-05-02 11:53:44 +0200

    Added IdentityDb, EFCore packages and auth/app layers

---
+++ Authentication/Contexts/IdentityUserDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Authentication.Entities;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace Authentication.Contexts
+{
+    public class IdentityUserDbContext(DbContextOptions<IdentityUserDbContext> options) : IdentityDbContext<AppUser>(options)
+    {
+
+    }
+}
--- Presentation/Program.cs
+++ Presentation/Program.cs
@@ -1,21 +1,29 @@
+using Authentication.Contexts;
+using Authentication.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllersWithViews();
+
+builder.Services.AddDbContext<IdentityUserDbContext>(x =>
+    x.UseSqlServer(builder.Configuration.GetConnectionString("VentixeIdentityDb")));
+builder.Services.AddIdentity<AppUser, IdentityRole>(x =>
+{
+    x.SignIn.RequireConfirmedAccount = true;
+    x.Password.RequiredLength = 8;
+    x.User.RequireUniqueEmail = true;
+}).AddEntityFrameworkStores<IdentityUserDbContext>().AddDefaultTokenProviders();
+//Configure application cookie here
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
+app.UseHsts();
 app.UseHttpsRedirection();
 app.UseRouting();
 
+app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapStaticAssets();
