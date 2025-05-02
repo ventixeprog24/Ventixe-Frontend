60f334224736fffc9b15e82b9ba9eef067bc78d9
Felix Cau <130475694+Felix-Cau@users.noreply.github.com>
2025-05-02 16:52:04 +0200

    Fixed lost method in AccountService

--- Application/Dtos/AccountServiceResult.cs
+++ Application/Dtos/AccountServiceResult.cs
@@ -1,9 +1,8 @@
 ﻿namespace Application.Dtos
 {
-    public class ServiceResult
+    public class AccountServiceResult
     {
         public bool Succeeded { get; set; }
-        public int StatusCode { get; set; }
         public string? Message { get; set; }
         public string? UserId { get; set; }
     }
--- Presentation/Controllers/SignUpController.cs
+++ Presentation/Controllers/SignUpController.cs
@@ -1,15 +1,47 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Application.Services;
+using Microsoft.AspNetCore.Mvc;
+using Presentation.Models.SignUp;
 using UserProfileServiceClient = UserProfileService.UserProfileService.UserProfileServiceClient;
 
 namespace Presentation.Controllers
 {
-    public class SignUpController(UserProfileServiceClient userProfileService) : Controller
+    public class SignUpController(UserProfileServiceClient userProfileService, AccountService accountService) : Controller
     {
         private readonly UserProfileServiceClient _userProfileService = userProfileService;
+        private readonly AccountService _accountService = accountService;
 
+        [HttpGet("signup")]
         public IActionResult Index()
         {
             return View();
+        }
+
+        [HttpPost("signup")]
+        public async Task<IActionResult> Index(SignUpViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                ViewBag.ErrorMessage = "Invalid email address";
+                return View(model);
+            }
+
+            var doesUsernameExist = await _accountService.FindByEmailAsync(model.Email);
+            if (doesUsernameExist.Succeeded)
+            {
+                ViewBag.ErrorMessage = "User already exists";
+                return View(model);
+            }
+
+            //This will send the verification code
+            var verificationResponse = await _verificationService.SendVerificationCodeAsync(model.Email);
+            if (!verificationResponse.Succeeded)
+            {
+                ViewBag.ErrorMessage = verificationResponse.Error;
+                return View(model);
+            }
+
+            TempData["Email"] = model.Email;
+            return RedirectToAction("AccountVerification");
         }
     }
 }
