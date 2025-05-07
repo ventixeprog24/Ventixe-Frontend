dffc2d6c0175142fd3eac9dc4ed0dc1a786ec517
AdamReinwalds <adam.reinvalds@gmail.com>
2025-05-07 18:15:05 +0200

    -

---
+++ Presentation/Controllers/LoginController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers;
+
+public class LoginController : Controller
+{
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
