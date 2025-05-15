515e385a43868e070de700c26522f4179067e1ce
Hanna282 <hanna.mickels@gmail.com>
2025-05-15 10:51:03 +0200

    Corrected account verification flow and model

--- Presentation/Models/SignUp/AccountVerificationViewModel.cs
+++ Presentation/Models/SignUp/AccountVerificationViewModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Presentation.Models.SignUp
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Presentation.Models.SignUp
 {
     public class AccountVerificationViewModel
     {
+        [Required(ErrorMessage = "Please enter the verification code")]
+        [StringLength(6, MinimumLength = 6, ErrorMessage = "The code must be 6 digits")]
         public string VerificationCode { get; set; } = null!;
+        public string? Email { get; set; } 
+        public string? VerificationToken { get; set; } 
     }
 }
