afae87312b1924227d078a6b1ab5b984e553cd2f
Hanna282 <hanna.mickels@gmail.com>
2025-05-21 13:43:49 +0200

    Add LocationService as client and base structure for location

---
+++ Presentation/Controllers/LocationController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Presentation.Controllers
+{
+    public class LocationController : Controller
+    {
+        [Route("Home/locations")]
+        public IActionResult Index()
+        {
+            ViewData["Title"] = "Locations";
+            return View();
+        }
+    }
+}
