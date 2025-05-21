0b7b76a4416b46a9fe767a3701f43745e52d65ac
emil hult <Emilhult@live.com>
2025-05-21 15:00:31 +0200

    New BookingService for Get, GetAll, Create and Delete

---
+++ Presentation/Factories/BookingModelFactory.cs
@@ -0,0 +1,51 @@
+﻿using BookingServiceProvider;
+using Presentation.Models.Bookings;
+
+namespace Presentation.Factories
+{
+    public class BookingModelFactory
+    {
+        public static BookingViewModel CreateBookingViewModel()
+        {
+            //return new BookingViewModel
+            //{
+            //    Id = booking.Bookingid,
+            //    UserId = booking.Userid,
+            //    EventId = booking.Eventid,
+            //    FirstName = booking.Firstname,
+            //    LastName = booking.Lastname,
+            //    Email = booking.Email,
+            //    PhoneNumber = booking.Phone,
+            //    Address = booking.Address,
+            //    PostalCode = booking.Postalcode,
+            //    City = booking.City,
+            //    EventName = booking.Eventname,
+            //    TicketAmount = booking.Ticketamount,
+            //    TicketPrice = booking.Ticketprice,
+            //    TotalPrice = booking.Totalprice,
+            //    EventDate = booking.Eventdate.ToDateTime(),
+            //    Created = booking.Created.ToDateTime()
+            //};
+            BookingViewModel bookingViewModel = new()
+            {
+                Id = "opkok",
+                UserId = "poajsa",
+                EventId = "poasjdpo",
+                FirstName = "pisjf",
+                LastName = "poasd",
+                Email = "posjfa",
+                PhoneNumber = "ipsjfpaos",
+                Address = "poajsd",
+                PostalCode = "posjfs",
+                City = "psjfa",
+                EventName = "opja",
+                TicketAmount = 2,
+                TicketPrice = 10,
+                TotalPrice = 20,
+                EventDate = DateTime.Now,
+                Created = DateTime.Now
+            };
+            return bookingViewModel;
+        }
+    }
+}
