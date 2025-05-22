471ca239d0285beceacaf848bf59edf522843d8b
emil hult <Emilhult@live.com>
2025-05-22 21:39:27 +0200

    Delete booking added

--- Presentation/Controllers/BookingController.cs
+++ Presentation/Controllers/BookingController.cs
@@ -23,7 +23,8 @@ namespace Presentation.Controllers
             return View(bookings.Bookings);
         }
 
-        [Route("Home/bookingdetails")]
+        [HttpGet("bookings/bookingdetails")] // lägg till ID
+        //[Route("bookins/bookingdetails")]
         public async Task<IActionResult> BookingDetails(string id)
         {
             var booking = await _bookingService.GetBookingAsync(id);
@@ -33,6 +34,44 @@ namespace Presentation.Controllers
             }
             ViewData["Title"] = "Booking Details";
             return View(booking.Booking);
+        }
+
+        public async Task<IActionResult> CreateBooking()
+        {
+            var booking = await _bookingService.GetAllBookings();
+            if (!booking.IsSuccess)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            ViewData["Title"] = "Create Booking";
+            return View(booking.Bookings);
+        }
+
+        public async Task<IActionResult> DeleteBooking(string id)
+        {
+            if (string.IsNullOrWhiteSpace(id))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            // DELETE BOOKING
+            var deleteResult = await _bookingService.DeleteBookingAsync(id);
+
+            if (!deleteResult.IsSuccess)
+            {
+                // GET BOOKING IF DELETE FAILED
+                var booking = await _bookingService.GetBookingAsync(id);
+                if (booking == null)
+                {
+                    TempData["ErrorMessage"] = "Failed to cancel booking, try again later.";
+                    return RedirectToAction("Index", "Booking");
+                }
+
+                ViewBag.ErrorMessage = "Failed to cancel booking, try again later.";
+                return View("BookingDetails", booking.Booking);
+            }
+
+            return RedirectToAction("Index", "Booking");
         }
     }
 }
